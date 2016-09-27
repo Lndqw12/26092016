502a07536111f40768409fc86e4d52572c081fb9
Lndqw12 <COMP222012016@mail.ru>
2016-09-27 23:00:08 +0300

    26092016js

---
+++ new.js
@@ -0,0 +1,19 @@
+﻿//function hypotenuse(a, b){
+	//function square(x) {return x*x;}
+	//return Math.sqrt(square(a) + square(b));
+	//printprops({x:1});
+	//var total = distance(0,0,2,1) + distance(2,1,3,5);
+	//var probability = factorial(5)/factorial(13);
+//	}
+var o ={
+	m:function() {
+		var self = this;
+		console.log(this === 0);
+		f();
+		function f() {
+			console.log(this === 0);
+			console.log(self === 0);
+		}
+	}
+}
+o.m();
---
+++ new1.js
@@ -0,0 +1,18 @@
+﻿//function f(x) {
+	//console.log(x);
+	//arguments[0] = null;
+	//console.log(x);
+//}
+function sum(a) {
+	if (isArrayLike(a)){
+		var total = 0;
+		for(var i = 0; i < a.length; i++) {
+			var element = a[i];
+			if (element == null) continue;
+			if (isFinite(element)) total += element;
+			else throw new Error("sum():Все элементы должны быть числами");
+		}
+		return total;
+	}
+	else throw new Error("sum():аргумент должен быть массивом");
+}
